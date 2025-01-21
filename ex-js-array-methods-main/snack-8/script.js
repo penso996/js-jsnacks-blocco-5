365623a206443f5340e5ca689d7b7221ff60756e
penso996 <penso.andrea@outlook.it>
2025-01-21 17:53:41 +0100

    ex 8 done

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const marcoClass = students.find(function (student) {
+  return student.name === 'Marco Lanci';
+}).class;
+
+console.log(marcoClass);
