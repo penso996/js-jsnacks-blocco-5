ed9860d9d0e1e18f2a3eec5a7210c77482cb37a3
penso996 <penso.andrea@outlook.it>
2025-01-21 17:46:36 +0100

    ex 5 done

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNums = nums.filter(function (num) {
+    return num % 2 === 0;
+});
+
+console.log(evenNums);
