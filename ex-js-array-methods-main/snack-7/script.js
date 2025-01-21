90bb4caf0a3608a187defcebf7a3da385dcc1fa3
penso996 <penso.andrea@outlook.it>
2025-01-21 17:51:57 +0100

    ex 7 done

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const studentId2 = students.find(function (student) {
+  return student.id === 2;
+});
+
+console.log(studentId2);
