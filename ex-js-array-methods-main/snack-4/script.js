775370e661da3317f797efb59eaa27f0cb19ce03
penso996 <penso.andrea@outlook.it>
2025-01-21 17:45:18 +0100

    ex 4 done

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const namesOnly = posts.map(function (post) {
+  return post.author;
+});
+
+console.log(namesOnly);
